6d5bce22f10f51148820e233d700fc4a7457ac9c
duckyduckg <duckyduckg65@gmail.com>
2025-04-20 07:36:06 +0500

    drivers: input: nvt36xxx: disable debugging * on aftermarket displays with novatek display and focaltech touchscreen, driver outputs errors on errors  regarding DT2W and other issues * this leads to bad logcat spam, so disable it as there is no way to fix DT2W or other issues on aftermarket hybrid displays
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/input/touchscreen/nt36xxx/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -58,7 +58,12 @@
 #else
 #define NVT_LOG(fmt, args...)    pr_debug("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
 #endif
+
+#if NVT_DEBUG
 #define NVT_ERR(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
+#else
+#define NVT_ERR(fmt, args...)    pr_debug("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
+#endif
 
 //---Input device info.---
 #define NVT_TS_NAME "NVTCapacitiveTouchScreen"
